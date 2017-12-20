80738c39c0f7e58e36d71376c0d5b0dacc9ff5e4
daniel <daniel.g_machado@hotmail.com>
2017-12-20 08:41:17 -0200

    Exclusão de backup MVC 4

---
+++ MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_.Startup))]
+namespace MVC_
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
